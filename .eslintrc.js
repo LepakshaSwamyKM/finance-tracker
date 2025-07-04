7aa17780205af0dfdfa536ef54e67a332a9c6624
lepaksha <lepakshaswamy60@gmail.com>
2025-07-05 02:16:40 +0530

    Disable ESLint rules for Vercel deployment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
+// .eslintrc.js
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-    extends: ['next', 'next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off', // ❌ disables the error
-      'prefer-const': 'off', // Optional: suppress const error
-    },
-  };
-  
+  extends: ["next", "next/core-web-vitals"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "prefer-const": "off",
+  },
+};
